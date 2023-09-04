f05ec23f8766dbe3b866a0686d14ed16f9bf0289
rengger <qaz5059@naver.com>
2023-09-04 19:14:17 +0900

    him

--- app.js
+++ app.js
@@ -1,7 +1,15 @@
 const express = require('express');
 const helmet = require('helmet');
 const app = express();
+const ejs = require("ejs");
+
+app.set('view engine', 'ejs');
+app.set('views', './views');
+app.use('/public', express.static(__dirname + '/public'));
+
 app.use(helmet());
+app.use(express.json());
+app.use(express.urlencoded());
 
 const mainRouter = require('./router/mainRouter');
 app.use('/', mainRouter);
--- router/mainRouter.js
+++ router/mainRouter.js
@@ -2,11 +2,18 @@ const express = require('express');
 const router = express.Router();
 
 router.get("/", function(req, res){
-    res.send('Hello World');
+    res.render('index', {title:"EJS 메인페이지"});   //그림을 전달할 때는 render
 })
 
 router.get("/about", function(req, res){
     res.send('About Page');
+})
+
+router.post("/postapi", function(req, res){
+    let body = req.body;
+    console.log(body);
+
+    res.send('POST API');
 })
 
 module.exports = router;
